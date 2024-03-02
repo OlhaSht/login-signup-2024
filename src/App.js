8262a0e9d8a7fbf1742b34febe7a34a81ee8bb85
Olha Shtepa <olgaolga2414@gmail.com>
2024-03-02 21:04:32 +0200

    create route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./static/images/logo.png";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './page/Home';
+import Login from './page/Login';
+import SineUp from './page/SineUp';
+import Header from './components/Header/index';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='login' element={<Login />} />
+      <Route path='sineup' element={<SineUp />} />
+    </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
